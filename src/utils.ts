ab0700b06184a5589569c86cc9fcb46fccbf1335
DonutLaser <vidmantas.luneckas@gmail.com>
2021-07-29 09:19:25 +0300

    .

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,5 @@
 import type { Rect } from "./types";
 
-export function clamp(value: number, min: number, max: number) {
-    if (value < min) { return min; }
-    if (value > max) { return max; }
-
-    return value;
-}
-
 export function getElementRect(element: HTMLElement): Rect {
     return {
         x: parseInt(trimFromEnd(element.style.left, 2), 10),
